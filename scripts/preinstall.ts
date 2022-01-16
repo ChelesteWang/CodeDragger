6604296d05e2ec277b5f4f4b848c72d61a8b2ca9
yuqing521 <1581446178@qq.com>
2022-01-16 16:00:10 +0800

    feat: add preinstall script

---
+++ scripts/preinstall.ts
@@ -0,0 +1,20 @@
+import chalk from 'chalk'
+const { yellow } = chalk
+
+// Test current package management tools
+
+if (!/pnpm/.test(process.env.npm_execpath || '')) {
+  console.warn(
+    yellow(
+      `[monorepo-tools]:Testing the current unused PNPM as a package management tool.\n` +
+        `[monorepo-tools]:Please install PNPM as a package manager.`
+    )
+  )
+  process.exit(1)
+}
+
+console.warn(
+  yellow(
+    `[monorepo-tools]:Testing the current used PNPM as a package management tool. continue...`
+  )
+)
