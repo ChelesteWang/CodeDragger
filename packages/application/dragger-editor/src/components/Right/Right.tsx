e671f48554e82ebf13590d0971b6fd8222a1fc78
ChelesteWang <1581446178@qq.com>
2022-01-20 10:22:14 +0800

    fix(dragger-editor): some layout optimization

--- packages/application/dragger-editor/src/components/Right/Right.tsx
+++ packages/application/dragger-editor/src/components/Right/Right.tsx
@@ -1,82 +1,83 @@
 import React from 'react'
-import ControlledAccordions from './component/ControlledAccordions/ControlledAccordions'
-import IconButtons from './component/IconButtons/IconButtons'
-import { TextField } from '@mui/material'
-import Button from '@mui/material/Button'
+// import ControlledAccordions from './component/ControlledAccordions/ControlledAccordions'
+// import IconButtons from './component/IconButtons/IconButtons'
+// import { TextField } from '@mui/material'
+// import Button from '@mui/material/Button'
 import './Right.css'
 
 export default class Right extends React.Component {
   render(): React.ReactNode {
     return (
-      <div className='right'>
-        <div className='top-btn'>
-          <Button variant='outlined'>button</Button>
-        </div>
-        <div className='small-icon'>
-          <IconButtons />
-        </div>
-        <div className='props'>
-          <ul className='props-list'>
-            <li>
-              <div>
-                <div>Text</div>
-                <TextField
-                  size='small'
-                  id='outlined-basic'
-                  variant='outlined'
-                />
-              </div>
-            </li>
-            <li>
-              <div>
-                <div>Size</div>
-                {/* <BasicSelect /> */}
-                <select name='' id=''></select>
-              </div>
-            </li>
-            <li>
-              <div>
-                <div>Variant</div>
-                {/* <BasicSelect/> */}
-                <select name='' id=''></select>
-              </div>
-            </li>
-            <li>
-              <div>
-                <div>Color Scheme</div>
-                <TextField
-                  size='small'
-                  id='outlined-basic'
-                  variant='outlined'
-                />
-              </div>
-            </li>
-            <li>
-              <div>
-                <div>Left icon</div>
-                <TextField
-                  size='small'
-                  id='outlined-basic'
-                  variant='outlined'
-                />
-              </div>
-            </li>
-            <li>
-              <div>
-                <div>Right icon</div>
-                <TextField
-                  size='small'
-                  id='outlined-basic'
-                  variant='outlined'
-                />
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <ControlledAccordions />
-        </div>
-      </div>
+      <div className='right'></div>
+      // <div className='right'>
+      //   <div className='top-btn'>
+      //     <Button variant='outlined'>button</Button>
+      //   </div>
+      //   <div className='small-icon'>
+      //     <IconButtons />
+      //   </div>
+      //   <div className='props'>
+      //     <ul className='props-list'>
+      //       <li>
+      //         <div>
+      //           <div>Text</div>
+      //           <TextField
+      //             size='small'
+      //             id='outlined-basic'
+      //             variant='outlined'
+      //           />
+      //         </div>
+      //       </li>
+      //       <li>
+      //         <div>
+      //           <div>Size</div>
+      //           {/* <BasicSelect /> */}
+      //           <select name='' id=''></select>
+      //         </div>
+      //       </li>
+      //       <li>
+      //         <div>
+      //           <div>Variant</div>
+      //           {/* <BasicSelect/> */}
+      //           <select name='' id=''></select>
+      //         </div>
+      //       </li>
+      //       <li>
+      //         <div>
+      //           <div>Color Scheme</div>
+      //           <TextField
+      //             size='small'
+      //             id='outlined-basic'
+      //             variant='outlined'
+      //           />
+      //         </div>
+      //       </li>
+      //       <li>
+      //         <div>
+      //           <div>Left icon</div>
+      //           <TextField
+      //             size='small'
+      //             id='outlined-basic'
+      //             variant='outlined'
+      //           />
+      //         </div>
+      //       </li>
+      //       <li>
+      //         <div>
+      //           <div>Right icon</div>
+      //           <TextField
+      //             size='small'
+      //             id='outlined-basic'
+      //             variant='outlined'
+      //           />
+      //         </div>
+      //       </li>
+      //     </ul>
+      //   </div>
+      //   <div>
+      //     <ControlledAccordions />
+      //   </div>
+      // </div>
     )
   }
 }
