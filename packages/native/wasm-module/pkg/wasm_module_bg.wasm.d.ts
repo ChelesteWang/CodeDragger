34af036f14c3a6b1bf1399ec6a1be73e877ab267
ChelesteWang <1581446178@qq.com>
2022-01-21 21:52:37 +0800

    chore: format code

--- packages/native/wasm-module/pkg/wasm_module_bg.wasm.d.ts
+++ packages/native/wasm-module/pkg/wasm_module_bg.wasm.d.ts
@@ -1,7 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function hello(a: number, b: number): void;
-export function strlen(a: number, b: number): number;
-export function __wbindgen_malloc(a: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const memory: WebAssembly.Memory
+export function hello(a: number, b: number): void
+export function strlen(a: number, b: number): number
+export function __wbindgen_malloc(a: number): number
+export function __wbindgen_realloc(a: number, b: number, c: number): number
