e8123a25ad5d57b98779d0a0b42476373db4c317
ChelesteWang <1581446178@qq.com>
2022-01-24 00:22:19 +0800

    feat(sandbox): add sandbox pakage

---
+++ packages/runtime/sandbox/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import './styles.css'
+import SandBox from './SandBox'
+
+function App() {
+  return (
+    <div className='App'>
+      <SandBox />
+    </div>
+  )
+}
+
+export default App
---
+++ packages/runtime/sandbox/src/SandBox.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, useRef } from 'react'
+import _debounce from 'lodash/debounce'
+import { createEditor } from './utils'
+// import MonacoEditor from "react-monaco-editor";
+
+function SandBox() {
+  const viewRef = useRef(null)
+  const runtimeRef = useRef<typeof createEditor>(null)
+  const [code, setCode] = useState(`
+    function Components() {
+      return <Hello message="React" />
+    }
+    function Hello({message}){
+      return <span>Hello,{message}</span>
+    }
+  `)
+
+  useEffect(() => {
+    runtimeRef.current = createEditor(viewRef.current)
+    runtimeRef.current.run(code)
+    console.log('compile', runtimeRef.current.compile(code))
+    console.log(
+      'generate CompiledCode',
+      runtimeRef.current.getCompiledCode(code)
+    )
+  }, [])
+
+  // 热更新 每 0.5 秒更新一次
+  const run = _debounce((newCode) => {
+    runtimeRef.current.run(newCode || code)
+  }, 500)
+
+  // handle onChange
+  const onCodeChange = ({ target: { value } }) => {
+    setCode(value)
+    run(value)
+  }
+
+  return (
+    <div className='container' style={{ display: 'flex' }}>
+      <div className='code-editor' style={{ flex: 1 }}>
+        <textarea value={code} onChange={onCodeChange} />
+      </div>
+      <div style={{ flex: 1 }}>
+        <div className='preview' ref={viewRef} />
+      </div>
+    </div>
+  )
+}
+
+export default SandBox
