099a38cda02e5e5413c079930b6d1250f6c5828a
m0_46688383 <760560917@qq.com>
2022-01-26 00:01:57 +0800

    feat(main-server): add jsonSchema-server

---
+++ packages/server/main-server/src/services/JsonSchemaService.js
@@ -0,0 +1,75 @@
+// 导出 Service 的实例
+module.exports = new JsonSchemaService();
+const jsonSchemaTable = require('../models/JsonSchemaTable');
+const inspirecloud = require('@byteinspire/api');
+const ObjectId = inspirecloud.db.ObjectId;
+/**
+ *  JsonSchemaService
+ * Service 是业务具体实现，由 Controller 或其它 Service 调用
+ * 提供jsonSchema的数据库操作功能
+ */
+
+class JsonSchemaService {
+    /**
+     * 列出所有JsonSchema
+     * @return {Promise<Array<any>>} 返回JsonSchema数组
+     */
+    async listAll() {
+        return await jsonSchemaTable.where().find();
+    }
+
+    /**
+     * 创建一条JsonSchema
+     * @param jsonSchema 用于创建JsonSchema的数据，原样存进数据库
+     * @return {Promise<any>} 返回实际插入数据库的数据，会增加_id，createdAt和updatedAt字段
+     */
+    async create(jsonSchema) {
+        if(!jsonSchema.user){
+            const error = new Error(`user not found`);
+            error.status = 401;
+            throw error;
+        }
+        return await jsonSchemaTable.save(jsonSchema);
+    }
+
+    /**
+     * 删除一条JsonSchema
+     * @param id JsonSchema的 _id
+     * 若不存在，则抛出 404 错误
+     */
+    async delete(id) {
+        const result = await jsonSchemaTable.where({_id: ObjectId(id)}).delete();
+        if (result.deletedCount === 0) {
+            const error = new Error(`jsonSchema:${id} not found`);
+            error.status = 404;
+            throw error;
+        }
+    }
+
+    /**
+     * 删除所有JsonSchema
+     */
+    async deleteAll() {
+        await jsonSchemaTable.where().delete();
+    }
+
+    /**
+     * 更新一条JsonSchema
+     * @param id JsonSchema的 _id
+     * @param updater 将会用原对象 merge 此对象进行更新
+     * 若不存在，则抛出 404 错误
+     */
+    async update(id, updater) {
+        const jsonSchema = await jsonSchemaTable.where({_id: ObjectId(id)}).findOne();
+        if (!jsonSchema) {
+            const error = new Error(`jsonSchema:${id} not found`);
+            error.status = 404;
+            throw error;
+        }
+        Object.assign(jsonSchema, updater);
+        await jsonSchemaTable.save(jsonSchema);
+    }
+}
+
+// 导出 Service 的实例
+module.exports = new JsonSchemaService();
