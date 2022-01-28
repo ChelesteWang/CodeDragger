ed4068f06fa454c25350344d3214d11010157bbc
m0_46688383 <760560917@qq.com>
2022-01-28 15:25:11 +0800

    fix(main-server): fix jsonSchema save bug

--- packages/server/main-server/src/controllers/JsonSchemaController.js
+++ packages/server/main-server/src/controllers/JsonSchemaController.js
@@ -31,7 +31,7 @@ class JsonSchemaController {
      */
     async create(ctx) {
         const {name,json} = ctx.request.body;
-        const user=UserService.current(ctx);
+        const  { info:user }=await UserService.current(ctx);
         const result = await JsonSchemaService.create({name,json,user });
         ctx.body = {result};
     }
