5a18ee34cd5dff7786e68644f6f1bcd39f42bad6
m0_46688383 <760560917@qq.com>
2022-01-29 02:04:47 +0800

    refactor(main-server): refactor code and modify api method

--- packages/server/main-server/src/routers/FileRouter.js
+++ packages/server/main-server/src/routers/FileRouter.js
@@ -6,9 +6,10 @@ const fileController = require('../controllers/FileController')
 const multer = require('@koa/multer')
 const upload = multer()
 
+router.get('/:id', fileController.download)
+router.get('/getContent/:id', fileController.getContent)
 router.post('/upload', upload.single('file'), fileController.upload)
-router.post('/delete', fileController.delete)
-router.post('/download', fileController.download)
+router.delete('/delete', fileController.delete)
 // router.get('/compile', fileController.compile)
 
 module.exports = router
