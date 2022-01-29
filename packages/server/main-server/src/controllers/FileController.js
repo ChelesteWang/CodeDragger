380bc5c875743ece93c61452d4553b2446b5f908
lweijian <63062575+lweijian@users.noreply.github.com>
2022-01-29 11:35:27 +0800

    refactor(main-server): refactor code  (#28)
    refactor(main-server): refactor code and modify api method

--- packages/server/main-server/src/controllers/FileController.js
+++ packages/server/main-server/src/controllers/FileController.js
@@ -1,7 +1,8 @@
 const fileService = require('../services/FileService')
 const fs = require('fs')
+const { sep } = require('path')
 // const {build, compress} = require("@cdl-pkg/package-server");
-// const {join} = require("path");
+
 // const FileService = require("../services/FileService");
 // const {v1} = require("uuid");
 
@@ -25,38 +26,57 @@ class FileController {
   }
 
   /**
-   *  路由:/api/file/delete
+   *  路由:/api/file/delete/:id
    * 删除文件
-   * 请求参数需要携带 url(要删除文件的url)必选
    * @param ctx
    * @return {Promise<any>}
    */
   async delete(ctx) {
-    const { url } = ctx.request.body
-    const result = await fileService.delete(ctx, url)
+    const id = ctx.params.id
+    const result = await fileService.delete(ctx, id)
     ctx.body = { result }
   }
 
   /**
-   *  路由:/api/file/download
+   *  路由:/api/file/:id
    * 下载文件
    * 返回文件流
-   * 请求参数需要携带 url(要下载文件的url)必选
    * @param ctx
    * @return {Promise<any>}
    */
   async download(ctx) {
-    const { url } = ctx.request.body
-    const result = await fileService.download(ctx, url)
-    if (success) {
-      const temp = result.filePath.split('\\')
+    const id = ctx.params.id
+    const result = await fileService.download(ctx, id)
+    if (result.success) {
+      const temp = result.filePath.split(sep)
       const filename = temp[temp.length - 1]
-      ctx.set('filename', filename)
+      ctx.set('Content-Disposition', `attachment;fileName=${filename}`)
       ctx.body = fs.createReadStream(result.filePath)
     } else {
       ctx.body = { result }
     }
   }
+
+  /**
+   *  路由:/api/file/content/:id
+   * 获取文件内容
+   * 返回文件内容
+   * @param ctx
+   * @return {Promise<any>}
+   */
+  async getContent(ctx) {
+    const id = ctx.params.id
+    const { success, message, filePath } = await fileService.download(ctx, id)
+    if (!success) {
+      throw new Error(message)
+    }
+    const content = fs.readFileSync(filePath).toString('utf8')
+    ctx.body = {
+      success,
+      content
+    }
+  }
+
   // /**
   //  *  路由:/api/file/compile
   //  * react文件编译
