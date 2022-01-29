b74535e2132cd0ad9bc49bad53aed4c8383fdefe
ChelesteWang <1581446178@qq.com>
2022-01-30 00:39:38 +0800

    feat(remote-component): pass test

--- packages/components/remote-component/src/service.ts
+++ packages/components/remote-component/src/service.ts
@@ -1,13 +1,8 @@
-async function getRemoteComponent({ name }: { name: string }) {
-  const url =
-    'https://qcpnya.file.qingfuwucdn.com/file/3c8a44cf32bb98b4_1643214779409.js'
-  const result = await fetch(`${url}`).then((a) => {
-    if (!a.ok) {
-      throw new Error('Network response was not ok')
-    }
-    return a as unknown as string
-  })
-  return result
+import axios from 'axios'
+async function getRemoteComponent({ url }: { url: string }) {
+  const _url = `/api/file/content/${url}`
+  const res = await axios.get(_url).then((res) => res.data.content)
+  return res
 }
 
 export { getRemoteComponent }
