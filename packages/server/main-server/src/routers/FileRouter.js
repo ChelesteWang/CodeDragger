d764b2d6450123c5c9577b5bb80d0ad0593b9876
lweijian <63062575+lweijian@users.noreply.github.com>
2022-01-29 11:35:27 +0800

    refactor(main-server): refactor code  (#28)
    refactor(main-server): refactor code and modify api method

--- packages/server/main-server/src/routers/FileRouter.js
+++ packages/server/main-server/src/routers/FileRouter.js
@@ -1,25 +1,15 @@
 const Router = require('@koa/router')
-// Koa 的路由在被 use 时是无法指定前缀的, 需要在定义时就指定前缀
+
 const router = new Router({ prefix: '/file' })
 const fileController = require('../controllers/FileController')
 //解析上传文件
 const multer = require('@koa/multer')
-const { build, compress } = require('@cdl-pkg/package-server')
-const { join } = require('path')
 const upload = multer()
-const FileService = require('../services/FileService')
-router.post('/upload', upload.single('file'), fileController.upload)
-router.post('/delete', fileController.delete)
 
-router.get('/build', async (ctx) => {
-  try {
-    await build({ outDir: '/tmp/dist' })
-    const buffer = await compress(`${join(__dirname, '../../tmp/dist')}`)
-    const { url } = await FileService.upload(ctx, { name: 'test2.tgz', buffer })
-    ctx.body = { url }
-  } catch (e) {
-    ctx.body = e
-  }
-})
+router.get('/:id', fileController.download)
+router.get('/content/:id', fileController.getContent)
+router.post('/upload', upload.single('file'), fileController.upload)
+router.delete('/delete/:id', fileController.delete)
+// router.get('/compile', fileController.compile)
 
 module.exports = router
