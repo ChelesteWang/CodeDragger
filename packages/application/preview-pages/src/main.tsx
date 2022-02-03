83a97657e91f14ddeba2fae2cdb34d6b6f9d6ff7
rexjz <robbort.xu@gmail.com>
2022-02-03 10:06:42 +0800

    feat: 接受postmessage传参

--- packages/application/preview-pages/src/main.tsx
+++ packages/application/preview-pages/src/main.tsx
@@ -1,20 +1,34 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { renderNode, NodeData, renderComponents } from '../../../runtime/render';
 import {mockmessage4} from './mock.js';
 
 
+function validateData(data: NodeData[]) {
+  return true;
+}
+
 function receiveMessage(event: MessageEvent) {
   if(event.type !== 'cd_components_data') {
     return;
   }
+  if(!validateData(event.data)) {
+    return;
+  }
+  renderData(event.data);
 }
 
 window.addEventListener('message', receiveMessage);
 
-async function processData(data: NodeData[]){
-  const vDom =  await renderNode(mockmessage4[0]);
+async function renderData(data: NodeData[]){
+  const vDom =  await renderComponents(data)
+  console.log('vDom', vDom);
+  if(vDom) {
+    ReactDOM.render(
+      vDom,
+      document.getElementById('root')
+    )
+  }
 }
 
 (async () => {
