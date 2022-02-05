4753f746a847bc2db417c1e45e1034c9710fb82a
Xinyuan Wang <40495740+ChelesteWang@users.noreply.github.com>
2022-02-05 21:05:08 +0800

    Revert "wip: Feat draggable"

--- packages/application/dragger-editor/src/components/Left/Left.tsx
+++ packages/application/dragger-editor/src/components/Left/Left.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import TabPanel from './component/TabPanelProps/TabPanelProps'
-import Box from '@mui/material/Box'
-import { WithDraggable } from '@/utils/draggable'
 import './Left.css'
 
-const DraggableBox = WithDraggable('Box')(Box)
 export default class Left extends React.Component {
   render(): React.ReactNode {
     return (
@@ -13,19 +10,7 @@ export default class Left extends React.Component {
           <TabPanel />
         </div>
         <div className='component'>
-          <div>
-            <DraggableBox
-              sx={{
-                width: '100%',
-                height: '100%',
-                backgroundColor: 'primary.dark',
-                '&:hover': {
-                  backgroundColor: 'primary.main',
-                  opacity: [0.9, 0.8, 0.7]
-                }
-              }}
-            />
-          </div>
+          <div></div>
           <div></div>
           <div></div>
           <div></div>
