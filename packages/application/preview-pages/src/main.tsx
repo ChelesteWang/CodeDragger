9c8c35f0846a8097cf0b5d34cf72450d71591cbf
ChelesteWang <1581446178@qq.com>
2022-02-05 23:38:25 +0800

    chore: format code

--- packages/application/preview-pages/src/main.tsx
+++ packages/application/preview-pages/src/main.tsx
@@ -1,44 +1,36 @@
 import ReactDOM from 'react-dom'
 import './index.css'
-import { renderNode, NodeData, renderComponents } from '../../../runtime/render';
-import {mockmessage4} from './mock.js';
-
+import { renderNode, NodeData, renderComponents } from '../../../runtime/render'
+import { mockmessage4 } from './mock.js'
 
 function validateData(data: NodeData[]) {
-  return true;
+  return true
 }
 
 function receiveMessage(event: MessageEvent) {
-  if(event.type !== 'cd_components_data') {
-    return;
+  if (event.type !== 'cd_components_data') {
+    return
   }
-  if(!validateData(event.data)) {
-    return;
+  if (!validateData(event.data)) {
+    return
   }
-  renderData(event.data);
+  renderData(event.data)
 }
 
-window.addEventListener('message', receiveMessage);
+window.addEventListener('message', receiveMessage)
 
-async function renderData(data: NodeData[]){
-  const vDom =  await renderComponents(data)
-  console.log('vDom', vDom);
-  if(vDom) {
-    ReactDOM.render(
-      vDom,
-      document.getElementById('root')
-    )
+async function renderData(data: NodeData[]) {
+  const vDom = await renderComponents(data)
+  console.log('vDom', vDom)
+  if (vDom) {
+    ReactDOM.render(vDom, document.getElementById('root'))
   }
 }
 
-(async () => {
-  const vDom =  await renderComponents(mockmessage4)
-  console.log('vDom', vDom);
-  if(vDom) {
-    ReactDOM.render(
-      vDom,
-      document.getElementById('root')
-    )
+;(async () => {
+  const vDom = await renderComponents(mockmessage4)
+  console.log('vDom', vDom)
+  if (vDom) {
+    ReactDOM.render(vDom, document.getElementById('root'))
   }
 })()
-
