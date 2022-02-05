4d62ff002fdeab16b6d12e5cbd5e34928974098a
Guxuanye <1962965866@qq.com>
2022-02-05 18:11:45 +0800

    feat: add workspace component
    新增了workspace页面，由Nav和Card两个component组成

---
+++ packages/application/main-application/src/components/Workspace/components/Card/index.tsx
@@ -0,0 +1,37 @@
+import {FC} from 'react'
+import './index.css'
+
+interface childProps {
+  title: string
+  edittime: string
+}
+
+const Card: FC<childProps> = (props) => {
+  const {title, edittime} = props;
+  return (
+    <div>
+      <div className='card'>
+        <div className='card_img'>
+
+        </div>
+
+        <div className='card_info'>
+          <div className='card_icon'>
+            <img src="http://tva1.sinaimg.cn/large/005BcTWDly1gz0kbe7a7nj302s02st8m.jpg"/>
+          </div>
+          <div className='card_text'>
+            <div className='card_title'>
+              {title}
+              {/* Untitled */}
+            </div>
+            <div className='c_edit_time'>
+              {edittime}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
