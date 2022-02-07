893463deca572fd01683758674997024db4886e6
Huangyunze <973113137@qq.com>
2022-02-08 04:05:22 +0800

    chore: 重构layout状态

--- packages/application/dragger-editor/src/components/Mid/component/Preview.tsx
+++ packages/application/dragger-editor/src/components/Mid/component/Preview.tsx
@@ -1,8 +1,8 @@
-import React, { CSSProperties, useRef, useState } from 'react'
+import React, { CSSProperties, useState } from 'react'
 import { useDrop } from 'react-dnd'
 import { Layout, Responsive, WidthProvider } from 'react-grid-layout'
 const ResponsiveReactGridLayout = WidthProvider(Responsive)
-export interface ComponentType {
+export interface LayoutType extends Layout {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   component: React.FC<any>
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -13,47 +13,44 @@ const style: CSSProperties = {
   height: '100%'
 }
 export default function Preview() {
-  const componentList = useRef<ComponentType[]>([])
-  const [layouts, setLayout] = useState<Layout[]>([])
+  const [layouts, setLayout] = useState<LayoutType[]>([])
   const [, drop] = useDrop(() => ({
     accept: 'Draggable-Component',
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     drop: (item: { type: React.FC<any>; props: any }) => {
-      componentList.current.push({
-        component: item.type,
-        dProps: item.props
-      })
       setLayout((oldLayout) => [
         ...oldLayout,
         {
-          x: oldLayout.length * 3,
+          i: '' + oldLayout.length,
+          x: 0,
           y: Infinity,
           w: 375,
           h: 100,
-          i: new Date().getTime().toString()
+          component: item.type,
+          dProps: item.props
         }
       ])
     }
   }))
+
   return (
     <div style={style} ref={drop}>
       <ResponsiveReactGridLayout
         className='layout'
         rowHeight={1}
-        layouts={{ layouts }}
-        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
         margin={[0, 0]}
+        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
         cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 375 }}
         droppingItem={{ i: new Date().getTime().toString(), w: 375, h: 100 }}
         isDroppable
       >
-        {componentList.current.map((IComponent, ind) => {
-          const FnComponent = IComponent.component
+        {layouts.map((layout, ind) => {
+          const FnComponent = layout.component
           return (
-            <div key={ind} data-grid={layouts[ind]}>
+            <div key={layout.i} data-grid={layouts[ind]}>
               <FnComponent
                 style={{ width: '100%', height: '100%' }}
-                {...IComponent.dProps}
+                {...layout.dProps}
               />
             </div>
           )
