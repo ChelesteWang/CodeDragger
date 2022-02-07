5e1d0bea49231ce61b22ff29df4416322c984bdb
m0_46688383 <760560917@qq.com>
2022-02-07 22:41:35 +0800

    refactor(main-server): refactor code

--- packages/server/main-server/src/controllers/FileController.js
+++ packages/server/main-server/src/controllers/FileController.js
@@ -2,7 +2,6 @@ const fileService = require('../services/FileService')
 const fs = require('fs')
 const { basename } = require('path')
 
-
 /**
  * fileController
  * Controller 是业务入口，由 HTTP 路由解析后调用
@@ -65,8 +64,6 @@ class FileController {
       content
     }
   }
-
-
 }
 
 // 导出 Controller 的实例
