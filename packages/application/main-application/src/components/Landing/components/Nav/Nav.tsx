eac32264fc986dba39091e88051f25fb0e1b3e40
ChelesteWang <1581446178@qq.com>
2022-02-08 22:47:05 +0800

    feat(landing-page): 美化 logo

--- packages/application/main-application/src/components/Landing/components/Nav/Nav.tsx
+++ packages/application/main-application/src/components/Landing/components/Nav/Nav.tsx
@@ -1,23 +1,55 @@
 import React from 'react'
-import Button from '@mui/material/Button'
 import Link from '@mui/material/Link'
 import './Nav.css'
+import Box from '@mui/material/Box'
 
 export default class Hello extends React.Component {
   render(): React.ReactNode {
     return (
       <div className='nav-bar'>
-        <div className='logo'>CodeDragger LowCode</div>
+        <div className='logo'>
+          <img
+            width='250'
+            src='https://qckvp9.file.qingfuwucdn.com/file/0a96a0d609bbee22_1644330902648.png'
+            alt='lowcode logo'
+          />
+        </div>
         <ul className='button-list'>
           <li>
-            <Button variant='contained'>Join us</Button>
+            <Box
+              component='span'
+              sx={{
+                display: 'inline-block',
+                height: '50px',
+                lineHeight: '32px',
+                width: '160px',
+                textAlign: 'center',
+                mr: 2,
+                p: 1,
+                background: '#333333',
+                '&:hover': {
+                  opacity: 0.7
+                }
+              }}
+            >
+              <Link
+                color={'#f8f4f8'}
+                variant='h6'
+                underline='none'
+                href='./login'
+              >
+                {'登录注册'}
+              </Link>
+            </Box>
           </li>
           <li>
-            <Button variant='contained'>Github link</Button>
-          </li>
-          <li>
-            <Link href='./login' underline='none'>
-              <Button variant='outlined'>立即体验</Button>
+            <Link
+              href='https://github.com/ChelesteWang/CodeDragger'
+              underline='none'
+              variant='h6'
+              sx={{ color: '#000' }}
+            >
+              <Box sx={{ p: 1, mr: 2 }}>Github link</Box>
             </Link>
           </li>
         </ul>
