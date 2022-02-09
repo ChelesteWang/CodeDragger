07a22c1b4dbb942203364c29ee4819b3c08f7e38
rexjz <54502639+rexjz@users.noreply.github.com>
2022-02-10 06:16:46 +0800

    feat: mock update

--- packages/application/dragger-editor/src/components/Left/service.ts
+++ packages/application/dragger-editor/src/components/Left/service.ts
@@ -1,5 +1,48 @@
 import http from '@cdl-pkg/request'
 
+const mockData = [
+  {
+    remoteComponent: true,
+    name: '62011592ec3be240902f635a',
+    desc: '按钮',
+    schema: {
+      "$schema": "http://json-schema.org/draft-04/schema#",
+      "$id": "https://codedragger.com/component/antd-mobile/Button",
+      "title": "codedagger/component/antd-mobile/Button",
+      "description": "antd-mobile Button attributes",
+      "type": "object",
+      "properties": {
+        "shape": {
+          "type": "string",
+          "oneof": ["rect" , "radius" , "round" , "circle"],
+          "default": "react",
+          "interaction": "drop_down"
+        },
+        "buttonText": {
+          "type": "string",
+          "default": "Button",
+          "interaction": "inupt"
+        },
+        "width": {
+          "type": "number",
+          "isCssProp": "true",
+          "default": 40,
+          "minimum": 40,
+          "maximum": 200,
+          "interaction": "stepper"
+        },
+        "height": {
+          "type": "number",
+          "isCssProp": "true",
+          "default": 80,
+          "minimum": 40,
+          "maximum": 200,
+          "interaction": "stepper"
+        }
+      }
+    }
+  }
+]
 export interface MaterialType {
   remoteComponent?: boolean;
   name?: string;
@@ -13,48 +56,6 @@ export interface GetMaterialListResponse {
 
 export function getMaterialList(): Promise<GetMaterialListResponse> {
   return Promise.resolve({
-    data: [
-      {
-        remoteComponent: true,
-        name: '62011592ec3be240902f635a',
-        desc: '按钮',
-        schema: {
-          "$schema": "http://json-schema.org/draft-04/schema#",
-          "$id": "https://codedragger.com/component/antd-mobile/Button",
-          "title": "codedagger/component/antd-mobile/Button",
-          "description": "antd-mobile Button attributes",
-          "type": "object",
-          "properties": {
-            "shape": {
-              "type": "string",
-              "oneof": ["rect" , "radius" , "round" , "circle"],
-              "default": "react",
-              "interaction": "drop_down"
-            },
-            "buttonText": {
-              "type": "string",
-              "default": "Button",
-              "interaction": "inupt"
-            },
-            "width": {
-              "type": "number",
-              "isCssProp": "true",
-              "default": 40,
-              "minimum": 40,
-              "maximum": 200,
-              "interaction": "stepper"
-            },
-            "height": {
-              "type": "number",
-              "isCssProp": "true",
-              "default": 80,
-              "minimum": 40,
-              "maximum": 200,
-              "interaction": "stepper"
-            }
-          }
-        }
-      }
-    ]
+    data: mockData
   });
 }
