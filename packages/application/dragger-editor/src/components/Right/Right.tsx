cd056630d2bf4c34e509e9bf1495c185bfdd5c54
ChelesteWang <1581446178@qq.com>
2022-02-09 22:51:03 +0800

    feat: 重构 Right 为 FC

--- packages/application/dragger-editor/src/components/Right/Right.tsx
+++ packages/application/dragger-editor/src/components/Right/Right.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 // import ControlledAccordions from './component/ControlledAccordions/ControlledAccordions'
 // import IconButtons from './component/IconButtons/IconButtons'
 // import { TextField } from '@mui/material'
 // import Button from '@mui/material/Button'
 import './Right.css'
 import ProfileForm from '@cdl-pkg/profile-form'
+import { Context } from '../../store'
 const selectedNode = {
   tag: 'MyCom',
   properties: {
@@ -24,81 +25,11 @@ const selectedNode = {
     }
   }
 }
-export default class Right extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <div className='right'>
-        <ProfileForm {...selectedNode} />
-      </div>
-      // <div className='right'>
-      //   <div className='top-btn'>
-      //     <Button variant='outlined'>button</Button>
-      //   </div>
-      //   <div className='small-icon'>
-      //     <IconButtons />
-      //   </div>
-      //   <div className='props'>
-      //     <ul className='props-list'>
-      //       <li>
-      //         <div>
-      //           <div>Text</div>
-      //           <TextField
-      //             size='small'
-      //             id='outlined-basic'
-      //             variant='outlined'
-      //           />
-      //         </div>
-      //       </li>
-      //       <li>
-      //         <div>
-      //           <div>Size</div>
-      //           {/* <BasicSelect /> */}
-      //           <select name='' id=''></select>
-      //         </div>
-      //       </li>
-      //       <li>
-      //         <div>
-      //           <div>Variant</div>
-      //           {/* <BasicSelect/> */}
-      //           <select name='' id=''></select>
-      //         </div>
-      //       </li>
-      //       <li>
-      //         <div>
-      //           <div>Color Scheme</div>
-      //           <TextField
-      //             size='small'
-      //             id='outlined-basic'
-      //             variant='outlined'
-      //           />
-      //         </div>
-      //       </li>
-      //       <li>
-      //         <div>
-      //           <div>Left icon</div>
-      //           <TextField
-      //             size='small'
-      //             id='outlined-basic'
-      //             variant='outlined'
-      //           />
-      //         </div>
-      //       </li>
-      //       <li>
-      //         <div>
-      //           <div>Right icon</div>
-      //           <TextField
-      //             size='small'
-      //             id='outlined-basic'
-      //             variant='outlined'
-      //           />
-      //         </div>
-      //       </li>
-      //     </ul>
-      //   </div>
-      //   <div>
-      //     <ControlledAccordions />
-      //   </div>
-      // </div>
-    )
-  }
+export default function Right() {
+  const { components, dispatch } = useContext(Context)
+  return (
+    <div className='right'>
+      <ProfileForm {...selectedNode} />
+    </div>
+  )
 }
