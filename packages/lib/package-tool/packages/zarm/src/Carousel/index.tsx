8864c884254d70f4f58f6cfd23b1767953189c83
ChelesteWang <1581446178@qq.com>
2022-02-10 00:11:30 +0800

    feat: 修改轮播图产物

--- packages/lib/package-tool/packages/zarm/src/Carousel/index.tsx
+++ packages/lib/package-tool/packages/zarm/src/Carousel/index.tsx
@@ -1,16 +1,10 @@
 import { Carousel } from 'zarm'
 
-// const ITEMS = [
-//   'https://static.zhongan.com/website/health/zarm/images/banners/1.png',
-//   'https://static.zhongan.com/website/health/zarm/images/banners/2.png',
-//   'https://static.zhongan.com/website/health/zarm/images/banners/3.png'
-// ]
-
 const contentRender = (items: string[]) => {
   return items.map((item, i) => {
     return (
       <div className='carousel__item__pic' key={+i}>
-        <img src={item} alt='' draggable={false} />
+        <img src={item} width='100%' alt='' draggable={false} />
       </div>
     )
   })
